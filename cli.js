6312a8b830164d68a64a752a666ca2eaccc0db81
Max Ogden <max@maxogden.com>
2016-12-31 13:56:22 -0800

    photocopier

--- cli.js
+++ cli.js
@@ -4,7 +4,10 @@ var photoImport = require('./')
 
 var from = process.argv[2]
 var to = process.argv[3]
-if (!from || !to) throw new Error('usage: photo-import <from> <to>')
+if (!from || !to) {
+  console.log('usage: photo-import <from> <to>')
+  process.exit(1)
+}
 
 photoImport(from, to, function (err, results) {
   if (err) throw err
